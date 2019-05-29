501992b37beef31abf2e401a60ba4d7e112bb843
Q.McKinley <q.mckinley@outlook.com>
2019-05-28 20:55:10 -0400

    streams update

--- streams-app/streams-client/src/components/streams/StreamEdit.js
+++ streams-app/streams-client/src/components/streams/StreamEdit.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStream, editStream } from '../../actions';
+import StreamFrom from './StreamForm';
 
-const SteamEdit = () => {
-  return <div>SteamEdit</div>
+class StreamEdit extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  };
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>
+    }
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamFrom 
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  };
 };
 
-export default SteamEdit;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
